6e8bb6ee9b12ddbe8099e957e1d2c855bb6d7fd0
asmaa mousa <asmaamousa978@gmail.com>
2023-04-17 00:15:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app=express();
+const students=["asmaa","aya","soha","mayada"]
+const studentfunction =(request,response)=>{
+ let output="<ul>";
+ for(let i=0;i<students.length;i++){
+    const student=students[i];
+    output+="<li>"+student+"</li>"
+ }
+
+ output+="</ul>";
+ response.send(output);
+
+};
+
+
+app.get("/students",studentfunction);
+app.listen(5000);
